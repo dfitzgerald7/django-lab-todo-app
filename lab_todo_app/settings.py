42d184e733492067941a2b6b538a86f635817c09
yourGitHubUserName <yourGitHub@email.com>
2019-05-01 17:00:56 -0400

    changed the static files back to the devcenter one

--- lab_todo_app/settings.py
+++ lab_todo_app/settings.py
@@ -126,7 +126,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
