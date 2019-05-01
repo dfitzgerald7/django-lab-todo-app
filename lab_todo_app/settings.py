7840dd8f322aa2bdc7ec1c70ea3f6acfadad3f0d
yourGitHubUserName <yourGitHub@email.com>
2019-05-01 16:20:15 -0400

    changed static to staticfiles in STATIC_ROOT

--- lab_todo_app/settings.py
+++ lab_todo_app/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
